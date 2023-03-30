24bc31bbc30c486f0015e184e63dea01c58fd821
chanwoo Hwang <chanwoo1017@hotmail.com>
2023-03-29 20:07:44 -0700

    change db

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize(process.env.JAWSDB_URL || process.env.LOCALDB_URL)
+module.exports = new Sequelize(process.env.MYSQL_DATABASE || process.env.LOCALDB_URL)
 
 
 
